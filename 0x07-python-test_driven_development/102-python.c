85fd63dcf2ee9489cba498f9e0839e792c0f46bd
Your Name <okorieralphc@gmail.com>
2024-02-01 07:08:01 -0800

    updated 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,5 +1,6 @@
-102-python.c
-#include "Python.h"
+#include <stdio.h>
+#include <string.h>
+#include <Python.h>
 
 /**
  * print_python_string - Prints information about Python strings.
@@ -26,4 +27,3 @@ void print_python_string(PyObject *p)
 		printf("  type: compact unicode object\n");
 	printf("  length: %ld\n", length);
 	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
-}
